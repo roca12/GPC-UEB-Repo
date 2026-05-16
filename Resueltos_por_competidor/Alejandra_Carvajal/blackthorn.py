e938ddbafacca14d2ff63e43451d9397b9cc350d
DynaMightt327 <malejacarvajalnepta@gmail.com>
2026-05-16 18:48:52 -0500

    Add blackthorn

---
+++ Resueltos_por_competidor/Alejandra_Carvajal/blackthorn.py
@@ -0,0 +1,14 @@
+# Autor: Alejandra Carvajal
+# Problema: Blackthorn
+# Juez online: BOCA
+# Veredicto: Accepted
+# URL: rpc4
+
+a = input()
+b = "kth"
+
+if b in a:
+    print("yes")
+else:
+    print("no")
+
