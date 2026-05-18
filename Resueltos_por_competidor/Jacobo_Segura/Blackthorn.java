64edb773aeb73a661be0754e7f7a3274fecfdd0f
Jacosex <juan.jacobo1208@gmail.com>
2026-05-18 11:59:40 -0500

    Create Blackthorn.java

---
+++ Resueltos_por_competidor/Jacobo_Segura/Blackthorn.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Blackthorn {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		if(s.contains("kth")) {
+			System.out.println("yes");
+			}else {
+				System.out.println("no");
+			}
+	}
+}
