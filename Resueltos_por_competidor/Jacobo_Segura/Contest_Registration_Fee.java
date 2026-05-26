65ec9ab350b0b69c2f4bd344811593c0b43c1d1f
Jacosex <juan.jacobo1208@gmail.com>
2026-05-25 21:51:56 -0500

    Create Contest_Registration_Fee.java

---
+++ Resueltos_por_competidor/Jacobo_Segura/Contest_Registration_Fee.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int x = sc.nextInt();
+	    int y = sc.nextInt();
+	    
+	    if(y<=x){
+	        System.out.println(0);
+	    }else{
+	        System.out.println(100);
+	    }
+	}
+}
