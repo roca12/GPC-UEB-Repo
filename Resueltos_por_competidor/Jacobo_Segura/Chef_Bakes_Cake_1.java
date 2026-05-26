aae70a6fe563a300d13bd5b264fdb113522cb65b
Jacosex <juan.jacobo1208@gmail.com>
2026-05-25 21:51:11 -0500

    Create Chef_Bakes_Cake_1.java

---
+++ Resueltos_por_competidor/Jacobo_Segura/Chef_Bakes_Cake_1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int np, nm, g;
+	    int n = sc.nextInt();
+	    int m = sc.nextInt();
+	    np = n*30;
+	    nm = m*50;
+	    
+	    g = nm-np;
+	    System.out.println(g);
+	}
+}
