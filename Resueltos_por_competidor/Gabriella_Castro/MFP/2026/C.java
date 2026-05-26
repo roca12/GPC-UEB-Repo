54ee8a0ab16a010e5c73198e2e2a4cc65cf68cbf
Gabriella Castro <159205699+gabGABY006@users.noreply.github.com>
2026-05-25 20:55:59 -0500

    Update C.java

--- Resueltos_por_competidor/Gabriella_Castro/MFP/2026/C.java
+++ Resueltos_por_competidor/Gabriella_Castro/MFP/2026/C.java
@@ -8,10 +8,10 @@
 
 //Hecho después de competencia
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class C {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,16 +20,14 @@ public class C {
         if (line != null) {
             StringTokenizer st = new StringTokenizer(line);
             
-            if (st.hasMoreTokens()) {
-                long x = Long.parseLong(st.nextToken());
-                long y = Long.parseLong(st.nextToken());
-                
-                long op1 = x * (y / 7);
-                
-                long op2 = (x / 7) * y;
-                
-                System.out.println(Math.max(op1, op2));
-            }
+            int χ = Integer.parseInt(st.nextToken());
+            int γ = Integer.parseInt(st.nextToken());
+ 
+            long op1 = (long) χ * (γ / 7);
+            
+            long op2 = (long) (χ / 7) * γ;
+ 
+            System.out.println(Math.max(op1, op2));
         }
     }
 }
