247d6bfef58a7caef82a01903d52e0370d4c74cf
Jacosex <juan.jacobo1208@gmail.com>
2026-05-25 21:47:44 -0500

    Create Pizza_Split.java

---
+++ Resueltos_por_competidor/Jacobo_Segura/Pizza_Split.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    if(n%2 == 0){
+	        System.out.println(1);
+	    }else{
+	        System.out.println(2);
+	    }
+	}
+}
