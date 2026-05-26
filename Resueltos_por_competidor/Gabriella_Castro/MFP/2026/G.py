3da5debcad15cf06a1e7ef8ff3a78d8d348140ae
Gabriella Castro <159205699+gabGABY006@users.noreply.github.com>
2026-05-25 21:00:02 -0500

    Create G.py

---
+++ Resueltos_por_competidor/Gabriella_Castro/MFP/2026/G.py
@@ -0,0 +1,7 @@
+# Autor: Gabriella Castro
+# Problema: Win or Lose?
+# Juez online: Codeforces
+# Veredicto: Accepted
+# URL: https://codeforces.com/group/9CNwiex6Ir/contest/693848/problem/G
+
+print("PERDE" if int(input()) % 3 == 0 else "GANHA")
