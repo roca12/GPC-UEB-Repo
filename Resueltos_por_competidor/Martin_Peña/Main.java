29b800aec58217e936d059bd2ea9ce6a874a0b44
Si <pgms1103@gmail.com>
2026-05-28 20:48:59 -0500

    Add files via upload

--- Resueltos_por_competidor/Martin_Peña/Main.java
+++ Resueltos_por_competidor/Martin_Peña/Main.java
@@ -1,9 +1,9 @@
 /*
 * Autor: pgms1103-afk
-* Problema: 4A - Watermelon
+* Problema: 71A - Way Too Long Words
 * Juez Online: Codeforces
 * Veredicto: Accepted
-* URL: https://codeforces.com/problemset/problem/4/A
+* URL: https://codeforces.com/problemset/problem/71/A
 */
 
 import java.io.BufferedReader;
@@ -12,19 +12,23 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-	public static void main(String[] args) throws IOException {
-
-		int w = Integer.parseInt(br.readLine());
-
-		if (w<=2){
-			System.out.println("NO");
-		}
-		else if (w % 2 == 0) {
-			System.out.print("YES");
-		} else {
-			System.out.print("NO");
+	static BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < n; i++) {
+			String palabra = br.readLine();
+			StringBuilder palabraSalida = new StringBuilder();
+			if(palabra.length() > 10) {
+				palabraSalida.append(palabra.charAt(0));
+				palabraSalida.append(palabra.length()-2);
+				palabraSalida.append(palabra.charAt(palabra.length()-1));
+				System.out.println(palabraSalida);
+			}else {
+				System.out.println(palabra);
+			}
 		}
 
 	}
