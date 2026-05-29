16c3710b589dfb4fe8c986f270f02bc8b3d6fb78
Si <pgms1103@gmail.com>
2026-05-28 20:55:19 -0500

    Add files via upload

---
+++ Resueltos_por_competidor/Martin_Peña/Watermelon.java
@@ -0,0 +1,32 @@
+/*
+* Autor: pgms1103-afk
+* Problema: 4A - Watermelon
+* Juez Online: Codeforces
+* Veredicto: Accepted
+* URL: https://codeforces.com/problemset/problem/4/A
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Watermelon {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+
+		int w = Integer.parseInt(br.readLine());
+
+		if (w<=2){
+			System.out.println("NO");
+		}
+		else if (w % 2 == 0) {
+			System.out.print("YES");
+		} else {
+			System.out.print("NO");
+		}
+
+	}
+
+}
