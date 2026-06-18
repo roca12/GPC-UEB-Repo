3308241ac1874780b269edc214644b2f546b51f2
Tomas <tomastrianag@gmail.com>
2026-06-18 00:04:06 -0500

    ComplicatedGCD.cpp

---
+++ Resueltos_por_competidor/Tomás_Triana_Galvis/ComplicatedGCD.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+/*
+ * Autor: Tomás Triana Galvis
+ * Problema: Complicated GCD
+ * Juez online: Codeforces664A
+ * Veredicto: Accepted
+ * Url: https://codeforces.com/contest/664/problem/A
+ **/ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string a,b;
+    cin>>a>>b;
+    if(a==b){
+        cout<<a<<endl;
+    }else{
+        cout<<"1"<<endl;
+    }
+    return 0;
+}
+
+
+
+
