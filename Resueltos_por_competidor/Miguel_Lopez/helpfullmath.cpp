d2043bdd7ee7fc7e31bfb29372b95f6de3cd082a
Fetchnt <delsinrowe752@gmail.com>
2026-06-27 16:32:57 -0500

    Add solution for Leapfrog Encryption problem
    Implement Leapfrog Encryption solution for CodeForces problem.

---
+++ Resueltos_por_competidor/Miguel_Lopez/helpfullmath.cpp
@@ -0,0 +1,46 @@
+/*
+ * Autor: Miguel Lopez
+ * Problema: 	Leapfrog Encryption 
+ * Juez online: CodeForces
+ * Veredicto: Accepted
+ * Url: https://codeforces.com/problemset/problem/339/A
+ */
+
+#include <bits/stdc++.h>
+typedef long long int ll;
+using namespace std;
+void init_code() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+}
+int main() {
+   init_code();
+    string a; cin >> a;
+    vector<int> num;
+    for(int i = 0; i < a.length(); i++) {
+        if(a[i] != '+') {
+            num.push_back(a[i] - '0');
+        }
+    }
+    sort(num.begin(), num.end());
+    int cont = 0;
+    for(int i = 0; i < a.length(); i++) {
+        if(a[i] != '+') {
+            a[i] = num[cont] + '0';
+        }
+        else {
+            cont++;
+        }
+    }
+    cout << a << endl;
+    return 0;
+
+ }
+
+
+
+ 
