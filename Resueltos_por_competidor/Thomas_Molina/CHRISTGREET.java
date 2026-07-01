085cc3d9e75d392c9241c51bb221d7b6a37d1b6a
thomas08095 <thomas08095@gmail.com>
2026-07-01 18:19:21 -0500

    Create CHRISTGREET.java

---
+++ Resueltos_por_competidor/Thomas_Molina/CHRISTGREET.java
@@ -0,0 +1,25 @@
+/*
+ * Autor: Thomas Molina
+ * Problema: CHRISTGREET
+ * Juez online: Codechef
+ * Veredicto: Accepted
+ * URL: https://www.codechef.com/problems/CHRISTGREET
+ */
+
+
+import java.util.*;
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int X;
+	    Scanner sc = new Scanner (System.in);
+	    X = sc.nextInt();
+	    if (X==25){
+	        System.out.println("CHRISTMAS");
+	    }
+	    else{
+	        System.out.println("ORDINARY");
+	    }
+	}
+}
