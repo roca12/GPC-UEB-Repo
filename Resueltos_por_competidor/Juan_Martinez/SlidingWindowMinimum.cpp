39d3d24249bc72d75292509916bd8cd2bce54889
Danrt <estehernandz2003@gmail.com>
2026-07-23 14:25:40 -0500

    Create SlidingWindowMinimum.cpp

---
+++ Resueltos_por_competidor/Juan_Martinez/SlidingWindowMinimum.cpp
@@ -0,0 +1,42 @@
+/*
+ * Autor: Juan Martinez
+ * Problema: Sliding Window Minimum
+ * Juez online: Cses
+ * Veredicto: Accepted
+ * Url: https://cses.fi/problemset/task/3221
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main() {
+	ll n, k;
+    ll x, a , b, c;
+    cin>>n>>k;
+    cin>>x>>a>>b>>c;
+    ll arr[n];
+
+    for(ll i = 0; i < n; i++) {
+        if(i == 0) arr[i] = x;
+        else arr[i] = ((a*arr[i-1]) + b) % c;
+    }
+    ll l = 0, res = 0, indice = 0;
+    deque<pair<ll, ll>> window;
+    for(ll i = 0; i < n; i++) {
+        while(!window.empty() && arr[i] < window.back().first) window.pop_back();
+        window.push_back({arr[i], i});
+        indice++;
+        if(indice >= k) {
+            //cout<<arr[l]<<" "<<arr[i]<<endl;
+            auto p = window.front();
+            while(!window.empty() && p.second < l){
+                window.pop_front();
+                p = window.front();
+            }
+            res ^= window.front().first;
+            l++;
+        }
+    }
+    cout<<res<<endl;
+}
