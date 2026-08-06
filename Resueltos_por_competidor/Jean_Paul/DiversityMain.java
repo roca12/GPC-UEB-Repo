78830031a51722c9846c592f5440eb4646fc5a06
Jean Quintero <jpquinterop08@gmail.com>
2026-08-06 18:59:57 -0500

    Create DiversityMain.java

---
+++ Resueltos_por_competidor/Jean_Paul/DiversityMain.java
@@ -0,0 +1,43 @@
+/*
+ *  Autor: Jean Paul
+ * Problema: A. ASCII Art Contest
+ * Juez online: Polygon 
+ * Veredicto: Accepted
+ * URL: https://codeforces.com/problemset/problem/2172/A
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class DiversityMain {
+ 
+	static Scanner sc = new Scanner(System.in);
+ 
+	public static void main(String[] args) {
+		String s = sc.next();
+ 
+		int k = sc.nextInt();
+ 
+		if (k > s.length()) {
+			System.out.println("impossible");
+		} else if (k == 1) {
+			System.out.println(0);
+		} else {
+			char[] chars = new char[s.length()];
+			for (int j = 0; j < s.length(); j++) {
+				chars[j] = s.charAt(j);
+			}
+ 
+			Arrays.sort(chars);
+			
+			int diff = 0;
+			for (int j = 0; j < s.length() - 1; j++) {
+				if (chars[j] != chars[j + 1]) {
+					diff++;
+				} 
+			}
+ 
+			System.out.println(k > diff? k-diff -1: 0);
+		}
+	}
+}
