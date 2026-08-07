dae6e16bdd0a56c5cec53fddaa7e011dcf24d641
Jacobo <juan.jacobo1208@gmail.com>
2026-08-07 11:33:56 -0500

    Create Elephant.java

---
+++ Resueltos_por_competidor/Jacobo_Segura/Elephant.java
@@ -0,0 +1,14 @@
+/* 
+Codeforces 617A - Elephant
+https://codeforces.com/problemset/problem/617/A 
+*/
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int p = (x+4)/5;
+		System.out.println(p);
+	}
+}
