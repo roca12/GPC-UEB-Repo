807d5d365abeed1e6faf81d482e3722b95e805ca
Jacobo <juan.jacobo1208@gmail.com>
2026-08-07 11:51:12 -0500

    Create Soldier_And_Bananas.java

---
+++ Resueltos_por_competidor/Jacobo_Segura/Soldier_And_Bananas.java
@@ -0,0 +1,17 @@
+/* 
+Codeforces 546A - Soldier and Bananas
+https://codeforces.com/problemset/problem/266/A
+*/
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long k = sc.nextLong(), n = sc.nextLong(), w = sc.nextLong();
+		long total = k*w*(w + 1) / 2;
+		long r = total - n;
+		
+		if(r < 0) r = 0;
+		System.out.println(r);
+	}
+}
