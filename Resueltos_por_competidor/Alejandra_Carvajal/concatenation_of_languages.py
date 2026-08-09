d5c0f1f3a9b96a379b55fdb476eed48c6c2ff0a0
DynaMightt327 <malejacarvajalnepta@gmail.com>
2026-08-08 19:40:09 -0500

    Create concatenation_of_languages.py

---
+++ Resueltos_por_competidor/Alejandra_Carvajal/concatenation_of_languages.py
@@ -0,0 +1,27 @@
+'''
+Autor: Alejandra Carvajal
+Problema: Concatenation of languages
+Juez Online: Codeforces
+Veredicto: Accepted
+URL: https://vjudge.net/problem/UVA-10887
+'''
+t = int(input())
+for case in range (t):
+
+    a, b = map(int, input().split())
+    palabra_uno = []
+    palabra_dos = []
+
+    for i in range(a):
+        aa = input()
+        palabra_uno.append(aa)
+    for i in range(b):
+        bb = input()
+        palabra_dos.append(bb)
+
+    palabra_tres = set()
+    for i in palabra_uno:
+        for j in palabra_dos:
+            palabra_tres.add(i + j)
+    
+    print(f'Case {case + 1}: {len(palabra_tres)}')
