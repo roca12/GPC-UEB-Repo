ffb660a45f95c50ef1fe650938825dcfa38b90e4
Shinrra_132K <124542111+Shinrra-132K@users.noreply.github.com>
2026-08-08 20:54:04 -0500

    Create Watermeloon.cpp

---
+++ Resueltos_por_competidor/Carlos_Cobaleda/Watermeloon.cpp
@@ -0,0 +1,18 @@
+/*
+	 * Autor: Carlos Eduardo Cobaleda Moreno
+	 * Problema: A. Watermelon
+	 * Juez online: CodeForces
+	 * Veredicto: Accepted
+	 * URL: https://codeforces.com/contest/4/problem/A
+	 */
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int w; cin >> w;
+    if (w > 2 && w%2 == 0) {
+    cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
