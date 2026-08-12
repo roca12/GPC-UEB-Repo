21b4a10aa65d3e66a6096b536d9b48883918c19d
Jacobo <juan.jacobo1208@gmail.com>
2026-08-12 08:14:35 -0500

    Create 2000.cpp

---
+++ Resueltos_por_competidor/Jacobo_Segura/2000.cpp
@@ -0,0 +1,10 @@
+//2000 on Codechef - https://www.codechef.com/problems/RIP2000
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int g = n*4;
+    cout<<g;
+}
