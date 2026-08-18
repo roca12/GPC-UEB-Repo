78ebcda3f951a2aecb428c8e87e7e56e57903b2f
Fetchnt <delsinrowe752@gmail.com>
2026-08-17 23:56:25 -0500

    mañana subo los links

---
+++ Resueltos_por_competidor/Miguel_Lopez/Copy.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        set<int> s;
+        while(n--) {
+            int a; cin >> a;
+            s.insert(a);
+        }
+        cout << s.size() << endl;
+    }
+}
+
