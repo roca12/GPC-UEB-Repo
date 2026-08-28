805c5907aeabd3d929daeaf3e2c162c8af05f636
Danrt <estehernandz2003@gmail.com>
2026-08-27 22:47:46 -0500

    Create 630A.cpp

---
+++ Resueltos_por_competidor/Juan_Martinez/630A.cpp
@@ -0,0 +1,15 @@
+/*
+ * Autor: Juan Martinez
+ * Problema: Again Twenty Five! (630A)
+ * Juez online: Codeforces
+ * Veredicto: Accepted
+ * Url: https://codeforces.com/problemset/problem/630/A
+ * Difficulty: 800
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.ignore();
+    cout<<25<<endl;
+}
