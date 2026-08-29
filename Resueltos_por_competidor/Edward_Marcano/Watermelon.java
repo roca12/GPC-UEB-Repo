b60db02a9aec0f4bc562c4812df04147f84996a5
EdwardMarcano77 <edward.marcano.2007@gmail.com>
2026-08-28 21:57:23 -0500

    Create Watermelon.java

---
+++ Resueltos_por_competidor/Edward_Marcano/Watermelon.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner (System.in);
+	    int t,w;
+	    w =sc.nextInt();
+
+	    if (w > 2  && w % 2 == 0) {
+	    	System.out.println("YES");
+	    }else {
+	    	System.out.println("No");
+	    }
+}
+
+	
+}
