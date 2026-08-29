cc0a91ef42bcc877695b4c53ec97ea8f0251761e
Jacobo <juan.jacobo1208@gmail.com>
2026-08-28 21:56:35 -0500

    Create I.java

---
+++ Resueltos_por_competidor/Jacobo_Segura/I.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt(), t = sc.nextInt();
+        int x = d-t;
+        if(x > 0) {
+        	System.out.println(x);
+        }else {
+        	System.out.println(0);
+        }
+    }
+}
