7090ef4d61d9c65e776a88e1d40c2e9bd9331262
Si <pgms1103@gmail.com>
2026-09-10 19:29:57 -0500

    Add files via upload

---
+++ Resueltos_por_competidor/Martin_Peña/BuildingRoads.java
@@ -0,0 +1,72 @@
+/*
+* Autor: pgms1103-afk
+* Problema: B. Two-gram
+* Juez Online: Codeforces
+* Veredicto: Accepted
+* URL: https://codeforces.com/contest/977/problem/B
+*/
+//ESTA SIN SUBIRRRRRR
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class BuildingRoads {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static ArrayList<Integer>[] callesConectadas;
+	static boolean[] visitado;
+	public static void main(String[] args) throws IOException {
+		
+		String datos[] = br.readLine().split(" ");
+		int numeroCiudades = Integer.parseInt(datos[0]);
+		int calles = Integer.parseInt(datos[1]);
+
+		callesConectadas = new ArrayList[numeroCiudades+1];	
+		
+		for (int i = 0; i <= numeroCiudades; i++) {
+			callesConectadas[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < calles; i++) {
+			String datos2[] = br.readLine().split(" ");
+			int u = (Integer.parseInt(datos2[0]));
+			int v = (Integer.parseInt(datos2[1]));
+			
+			callesConectadas[u].add(v);
+			callesConectadas[v].add(u);
+		}
+		
+		visitado = new boolean[numeroCiudades + 1];
+		ArrayList<Integer> representantes = new ArrayList<>();
+		int nuevasCarreteras = 0;
+		for (int i = 1; i <= numeroCiudades; i++) {
+			if(!visitado[i]) {
+				nuevasCarreteras++;
+				representantes.add(i);
+				dfs(i);	
+			}
+		}
+		
+		System.out.println(nuevasCarreteras-1);
+		if(nuevasCarreteras-1 != 0) {
+			for (int i = 0; i < representantes.size()-1; i++) {
+				int primero = representantes.get(i);
+				int segundo = representantes.get(i+1);
+				System.out.println(primero + " " + segundo);
+			}
+		}
+		
+	}
+	
+	static void dfs(int nodo) {
+		visitado[nodo] = true;
+		for (int vecino :  callesConectadas[nodo]) {
+			if(!visitado[vecino]) {
+				dfs(vecino);
+			}
+		}
+		
+	}
+
+}
---
+++ Resueltos_por_competidor/Martin_Peña/MessageRoute.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+public class MessageRoute {
+
+	static BufferedReader br = new BufferedReader (new InputStreamReader(System.in)); 
+	static boolean visitados[];
+	static ArrayList<Integer> compusConectados[];
+	static int numeroCompus;
+	static int[] padre;
+	static boolean llego = false;
+	public static void main(String[] args) throws IOException {
+		
+		
+		String datos[] = br.readLine().split(" ");
+		numeroCompus = Integer.parseInt(datos[0]);
+		int numeroConecciones = Integer.parseInt(datos[1]);
+		visitados = new boolean[numeroCompus+1];
+		
+		padre = new int[numeroCompus + 1];
+		compusConectados = new ArrayList[numeroCompus+1];
+		for (int i = 0; i < compusConectados.length; i++) {
+			compusConectados[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < numeroConecciones; i++) {
+			String quienConQuien[] = br.readLine().split(" ");
+			int a = Integer.parseInt(quienConQuien[0]);
+			int b = Integer.parseInt(quienConQuien[1]);
+			
+			compusConectados[a].add(b);
+			compusConectados[b].add(a);
+		}
+		
+		bfs(1);
+		
+		if(!visitados[numeroCompus]) {
+			System.out.println("IMPOSSIBLE");
+		}else {
+			ArrayList<Integer> rutaFinal = new ArrayList<>();
+			for (int i = numeroCompus; i != 0; i = padre[i] ) {
+				rutaFinal.add(i);
+			}
+			
+			System.out.println(rutaFinal.size());
+			for (int r = rutaFinal.size()-1; r >= 0; r--) {
+				System.out.print(rutaFinal.get(r) + " ");
+			}
+		}
+	}
+	
+	static void bfs (int inicio) {
+		ArrayDeque<Integer> cola = new ArrayDeque<>();	
+		cola.add(inicio);
+		visitados[inicio] = true;
+		
+		while(!cola.isEmpty()) {
+			int actual = cola.poll();
+			
+			for (int vecino: compusConectados[actual]) {
+					if(!visitados[vecino]) {
+						visitados[vecino] = true;
+						padre[vecino] = actual;
+						cola.add(vecino);
+					}
+			}
+		}
+	}
+	
+	
+
+}
